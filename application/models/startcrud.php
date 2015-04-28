40f816df4b5c9bc6090f6ec53f336295ce86bd99
smartinfreelance <smartinmedina@hotmail.com>
2015-04-28 20:05:54 -0300

    testTraining First Update
    Vamooo

---
+++ application/models/startcrud.php
@@ -0,0 +1,14 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Startcrud extends CI_Model {
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    function startit(){
+    	return "1";
+    }
+}
+?>
